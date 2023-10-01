c9b85f8313b0f95da5bc0990e47953caa250301e
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2023-09-30 21:58:49 -0400

    Basic binding of plain Go values

---
+++ bind/bind.go
@@ -0,0 +1,42 @@
+package bind
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/t0yv0/complang/value"
+)
+
+func Value(v any) value.Value {
+	switch v := v.(type) {
+	case nil:
+		return &value.NullValue{}
+	case value.Value:
+		return v
+	case reflect.Value:
+		return Value(v.Interface())
+	case string:
+		return &value.StringValue{Value: v}
+	case bool:
+		return &value.BoolValue{Value: v}
+	default:
+		vv := reflect.ValueOf(v)
+		switch {
+		case vv.Kind() == reflect.Slice || vv.Kind() == reflect.Array:
+			vs := []value.Value{}
+			for i := 0; i < vv.Len(); i++ {
+				vs = append(vs, Value(vv.Index(i)))
+			}
+			return &value.SliceValue{Value: vs}
+		case vv.Kind() == reflect.Map && vv.Type().Key().Kind() == reflect.String:
+			m := map[value.Symbol]value.Value{}
+			for _, key := range vv.MapKeys() {
+				m[value.NewSymbol(key.String())] = Value(vv.MapIndex(key))
+			}
+			return &value.MapValue{Value: m}
+		default:
+			return &value.ErrorValue{ErrorMessage: fmt.Sprintf(
+				"Cannot bind value of type #%T to complang yet: %#V", v, v)}
+		}
+	}
+}
--- cmd/complang-bare/main.go
+++ cmd/complang-bare/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/t0yv0/complang/bind"
 	"github.com/t0yv0/complang/repl"
 	"github.com/t0yv0/complang/value"
 )
@@ -18,6 +19,12 @@ func main() {
 					value.NewSymbol("three"): &value.StringValue{Value: "3"},
 				},
 			},
+			value.NewSymbol("$something"): bind.Value(map[string]interface{}{
+				"string": "stringValue",
+				"bool":   true,
+				"slice":  []string{"a", "b", "c"},
+				"map":    map[string]string{"one": "1"},
+			}),
 		},
 	})
 	if err != nil {
