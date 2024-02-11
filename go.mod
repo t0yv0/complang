f70735855815b19e04e833cf754ebfb3092f33ec
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2024-02-10 21:33:25 -0500

    Use liner

--- go.mod
+++ go.mod
@@ -3,17 +3,18 @@ module github.com/t0yv0/complang
 go 1.20
 
 require (
-	github.com/chzyer/readline v1.5.1
 	github.com/lithammer/fuzzysearch v1.1.8
+	github.com/peterh/liner v1.2.2
 	github.com/stretchr/testify v1.8.4
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-runewidth v0.0.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 replace github.com/chzyer/readline => github.com/t0yv0/readline v0.0.0-20230907050713-5bb033bdf64f
--- repl/repl.go
+++ repl/repl.go
@@ -1,10 +1,13 @@
 package repl
 
 import (
-	"fmt"
-
 	"context"
-	"github.com/chzyer/readline"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/peterh/liner"
 	cl "github.com/t0yv0/complang"
 	"github.com/t0yv0/complang/expr"
 	"github.com/t0yv0/complang/parser"
@@ -16,60 +19,90 @@ type ReadEvalPrintLoopOptions struct {
 	MaxCompletions     int
 }
 
-func ReadEvalPrintLoop(ctx context.Context, cfg ReadEvalPrintLoopOptions) error {
+func ReadEvalPrintLoop(ctx context.Context, cfg ReadEvalPrintLoopOptions) (finalError error) {
 	maxCompletions := cfg.MaxCompletions
+
 	if maxCompletions == 0 {
 		maxCompletions = 16
 	}
+
 	env := cl.NewMutableEnv()
+
 	for k, v := range cfg.InitialEnvironment {
 		env.Bind(k, v)
 	}
-	return readlineREPL(ctx, cfg.HistoryFile, &complangInterpreter{
-		ctx:            ctx,
-		env:            env,
-		maxCompletions: maxCompletions,
-	})
-}
 
-type complangInterpreter struct {
-	ctx            context.Context
-	env            cl.MutableEnv
-	maxCompletions int
-}
+	rliner := liner.NewLiner()
 
-func (ci *complangInterpreter) ReadEvalPrint(ctx context.Context, command string) {
-	stmt, err := parser.ParseStmt(command)
-	if err != nil {
-		fmt.Printf("ERROR invalid syntax: %v\n", err)
-		return
-	}
-	if stmt != nil {
-		expr.EvalStmt(ctx, ci.env, stmt)
-	}
-}
-
-func (ci *complangInterpreter) ReadEvalComplete(
-	partialCommand string,
-) []readline.Candidate {
-	query, err := parser.ParseQuery(partialCommand)
-	if err != nil {
-		return nil
-	}
-	if query == nil {
-		return nil
-	}
-
-	f := fuzzyComplete(ci.maxCompletions)
-	expr.EvalQuery(ci.ctx, ci.env, query, f.complete)
-
-	result := []readline.Candidate{}
-	for _, c := range f.matches(query.QueryText()) {
-		rc := readline.Candidate{
-			NewLine: []rune(partialCommand[0:query.Offset()] + c),
-			Display: []rune(c),
+	defer func() {
+		if err := rliner.Close(); err != nil {
+			finalError = errors.Join(finalError, err)
 		}
-		result = append(result, rc)
+	}()
+
+	rliner.SetCtrlCAborts(true)
+	rliner.SetTabCompletionStyle(liner.TabPrints)
+
+	rliner.SetWordCompleter(func(
+		line string, pos int,
+	) (head string, completions []string, tail string) {
+		head = line[0:pos]
+		completions = []string{}
+		tail = line[pos:]
+		queryText := line[0:pos]
+		query, err := parser.ParseQuery(queryText)
+		if err != nil {
+			return
+		}
+		f := fuzzyComplete(maxCompletions)
+		expr.EvalQuery(ctx, env, query, f.complete)
+		head = line[0:query.Offset()]
+		completions = f.matches(query.QueryText())
+		tail = ""
+		return
+	})
+
+	if cfg.HistoryFile != "" {
+		if f, err := os.Open(cfg.HistoryFile); err == nil {
+			_, err := rliner.ReadHistory(f)
+			if err != nil {
+				return fmt.Errorf("Error reading history file: %v", err)
+			}
+			if err := f.Close(); err != nil {
+				return fmt.Errorf("Error closing history file: %v", err)
+			}
+		}
+		defer func() {
+			var e error
+			if f, err := os.Create(cfg.HistoryFile); err != nil {
+				e = fmt.Errorf("Error creating history file: %w", err)
+			} else {
+				if _, err := rliner.WriteHistory(f); err != nil {
+					e = fmt.Errorf("Error writing history file: %w", err)
+				} else if err := f.Close(); err != nil {
+					e = fmt.Errorf("Error finalizing history file: %w", err)
+				}
+			}
+			finalError = errors.Join(finalError, e)
+		}()
 	}
-	return result
+
+	for {
+		if command, err := rliner.Prompt("> "); err == nil {
+			stmt, err := parser.ParseStmt(command)
+			if err != nil {
+				fmt.Printf("ERROR invalid syntax: %v\n", err)
+				continue
+			}
+			if stmt != nil {
+				expr.EvalStmt(ctx, env, stmt)
+				rliner.AppendHistory(command)
+			}
+		} else if err == liner.ErrPromptAborted || err == io.EOF {
+			fmt.Println("")
+			return nil
+		} else {
+			return fmt.Errorf("Error reading line: %w", err)
+		}
+	}
 }
